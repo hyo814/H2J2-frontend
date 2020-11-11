60124dfb35a1a18308529cd5c4859af3eee0c103
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2020-11-11 19:58:11 +0900

    Update CustomerDe.js

--- src/components/views/Customer/CustomerDe.js
+++ src/components/views/Customer/CustomerDe.js
@@ -1,4 +1,4 @@
-import React, {Component} from"react";
+import React, {Component} from "react";
 import Button from '@material-ui/core/Button';
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
@@ -8,22 +8,22 @@ import CustomerEdit from "./CustomerEdit";
 import cookie from 'react-cookies';
 
 
-class CustomerDe extends Component{
+class CustomerDe extends Component {
     state = {
         loading: false,
         details: [],
-    token:cookie.load('token'),
-    user_id:cookie.load('user_id')
+        token: cookie.load('token'),
+        userid: cookie.load('userid')
     };
 
     pwReset = async () => {
-        axios.post('http://h2j22020.vps.phps.kr/api/reset/pwd',{ headers: { Authorization: ` ${cookie.load('token')}` } })
-            .then(({ data }) => {
+        axios.post('http://h2j22020.vps.phps.kr/api/reset/pwd', {headers: {Authorization: ` ${cookie.load('token')}`}})
+            .then(({data}) => {
                 this.setState({
-                    loading: true,
-                    details: data,
-                },
-                this.getPosts());
+                        loading: true,
+                        details: data,
+                    },
+                    this.getPosts());
             })
             .catch(e => {
                 console.error(e);
@@ -32,32 +32,40 @@ class CustomerDe extends Component{
                 });
             });
     };
+
     componentDidMount() {
         this.pwReset();
     }
-    render(){
-        return(
-                        <TableRow>
-                            <TableCell align='center'> {this.props.student_id} </TableCell>
-                            <TableCell align='center'>{this.props.name} </TableCell>
-                            <TableCell align='center'>{this.props.user_id} </TableCell>
-                            <TableCell align='center'>{this.props.grade} </TableCell>
-                            <TableCell align='center'>{this.props.semester} </TableCell>
-                            <TableCell align='center'>{this.props.phone} </TableCell>
-                            <TableCell align='center'>{this.props.email} </TableCell>
-                            <TableCell align='center'>{this.props.rent} </TableCell>{/*내 도서 대출 페이지로 링크 이동*/}
-                            <TableCell align='center'>{this.props.level}</TableCell>
-                            <TableCell align='center'><Button variant="contained"
-                                                              color="primary">Reset</Button></TableCell>
-                            <TableCell align='center'><CustomerDelete stateRefresh={this.props.stateRefresh}
-                                                                      user_id={this.props.user_id}/></TableCell>
-                            <TableCell align='center'><CustomerEdit stateRefresh={this.props.stateRefresh}
-                                                                    user_id={this.props.user_id} name={this.props.name}
-                                                                    student_id={this.props.student_id}
-                                                                    grade={this.props.grade}
-                                                                    semester={this.props.semester}
-                                                                    level={this.props.level}/></TableCell>
-                        </TableRow>
+
+    render() {
+        return (
+            <TableRow>
+                <TableCell align='center'>{this.props.userid} </TableCell>
+                <TableCell align='center'>{this.props.name} </TableCell>
+                <TableCell align='center'>{this.props.phone} </TableCell>
+                <TableCell align='center'>{this.props.email} </TableCell>
+                <TableCell align='center'>{this.props.address} </TableCell>
+                <TableCell align='center'>{this.props.age} </TableCell>
+                <TableCell align='center'>{this.props.sex} </TableCell>
+                <TableCell align='center'>{this.props.height} </TableCell>
+                <TableCell align='center'>{this.props.weight} </TableCell>
+                <TableCell align='center'>{this.props.basic_metabolic} </TableCell>
+                <TableCell align='center'>{this.props.bmi} </TableCell>
+                <TableCell align='center'>{this.props.level}</TableCell>
+                <TableCell align='center'><Button variant="contained"
+                                                  color="primary">Reset</Button></TableCell>
+                <TableCell align='center'><CustomerDelete stateRefresh={this.props.stateRefresh}
+                                                          userid={this.props.user_id}/></TableCell>
+                <TableCell align='center'><CustomerEdit stateRefresh={this.props.stateRefresh}
+                                                        userid={this.props.userid} name={this.props.name}
+                                                        phone={this.props.phone}
+                                                        email={this.props.email} address={this.props.address}
+                                                        age={this.props.age} sex={this.props.sex}
+                                                        height={this.props.height} weight={this.props.weight}
+                                                        basic_metabolic={this.props.basic_metabolic}
+                                                        bmi={this.props.bmi}
+                                                        level={this.props.level}/></TableCell>
+            </TableRow>
         );
     }
 }
