bb37cf78e822ec8c06dd00300db8c36dbf161dce
juHyun_dev <61489150+juHyunKim-dev@users.noreply.github.com>
2020-11-22 02:41:42 +0900

    Add files via upload

---
+++ src/components/views/Fassion/WomenSS/WSSpage.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import cookie from "react-cookies";
+import { Container, Row, Col } from 'reactstrap';
+
+class WSSpage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user_id:cookie.load('user_id')
+        }
+        console.log("user_id: "+this.state.user_id)
+    }
+
+    render() {
+        const { Clothes } = this.props;
+        return (
+            <Container>
+                <div class="intro">
+                <p><h1>패션도 놓칠 수 없죠<br/></h1></p>
+                "내가 하는 운동에 적합한 옷을 입는 것도 중요해요.<br/>
+                큐피트가 추천하는 패션 아이템을 살펴보세요!"
+                </div>
+                <Row>
+                    { Clothes.map((cloth) => {
+                        return (
+                            <Col xs="3">
+                                <p><a href={cloth.url}><img src={cloth.img_src} width="200px" height="200px" alt="" /></a></p>
+                                <div class="name">{cloth.name}</div>
+                                <div id="price">{cloth.price}원</div>
+                                <br/><br/>
+                            </Col>
+                        )
+                    })}
+                </Row>
+
+            </Container>
+
+        );
+    }
+}
+
+export default WSSpage
