4ef092bcc2d9e229710d890985bcecb8d101639b
hyo814 <ggamjige8888@naver.com>
2020-11-22 14:07:09 +0900

    페이지!

--- src/components/PaginationButton.js
+++ src/components/PaginationButton.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { withRouter } from 'react-router-dom';
+import {Button} from 'reactstrap';
+import styled from 'styled-components';
 
 const styles = theme => ({
     button: {
@@ -10,26 +12,29 @@ const styles = theme => ({
 class PaginationButton extends Component {
     render() {
         return (
-            <>
+            <Div>
                 <span className="page">
-                    {this.props.currentPage!==1?<button variant="outlined" color="primary" onClick={() => this.props.onClick(this.props.currentPage-1)}>
+                    {this.props.currentPage!==1?<Button  outline color="primary" onClick={() => this.props.onClick(this.props.currentPage-1)}>
                         {"<"}
-                    </button>:""}
+                    </Button>:""}
                     {[...Array(this.props.page)].map((n, index) => {
-                        return <button className={this.props.currentPage === n+1 ? "page-item active" : "page-item"}
+                        return <Button className={this.props.currentPage === n+1 ? "page-item active" : "page-item"}
                                        onClick={() => this.props.onClick(index+1)}
-                                       variant="outlined"
-                                       color="primary">
+                                       outline color="primary">
                             {index+1}
-                        </button>
+                        </Button>
                     })}
-                    {this.props.page!==this.props.currentPage?<button variant="outlined" color="primary" onClick={() => this.props.onClick(this.props.currentPage+1)}>
+                    {this.props.page!==this.props.currentPage?<Button  outline color="primary" onClick={() => this.props.onClick(this.props.currentPage+1)}>
                         {">"}
-                    </button>:""}
+                    </Button>:""}
                 </span>
-            </>
+            </Div>
         );
     }
 }
+
+const Div=styled.div`
+margin:1% auto;
+`;
 
 export default withRouter(PaginationButton);
